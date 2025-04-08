9115c0f137cf6b85d2ded2d9a9e4797ad3027e74
Rosadev Solutions | Dev Romulo Rosa <rosadevsolutions@gmail.com>
2025-04-07 21:07:55 -0300

    fazer login forma 1

--- codigo.py
+++ codigo.py
@@ -1,5 +1,3 @@
-#PASSO 2 - Fazer Login (qq EMAIL E SENHA)
-
 #PASSO 3 - Importar Base de Dados
 
 #PASSO 4 - Abrir Base de Dados
@@ -14,10 +12,26 @@
 #importamos a lib de automatação
 import pyautogui
 
+#lib parra controle das execuções
+import time
+
+#Definir um delay entre os comandos
+pyautogui.PAUSE = 0.35
+
 #PASSO 1 - Entrar no sistema - https://dlp.hashtagtreinamentos.com/python/intensivao/login
 pyautogui.press("win")
 pyautogui.write("chrome")
 pyautogui.press("enter")
-pyautogui.press("search")
 pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
+pyautogui.press("enter")
+
+#chamar a lib time com o metodo sleep pra definir tempo de espera pra carregar a página
+time.sleep(1)
+
+#PASSO 2 - Fazer Login (qq EMAIL E SENHA)
+pyautogui.press("tab")
+pyautogui.write("romulo.ilearn@gmail.com")
+pyautogui.press("tab")
+pyautogui.write("Admin#13")
+pyautogui.press("tab")
 pyautogui.press("enter")
